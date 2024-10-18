6a422e6ced434f7ad6e34e134c3375122d90ea88
Ольга <o-style@bk.ru>
2024-10-18 15:33:23 +0300

    ToDo List with (un)complete options // h/work for fifth (1 half) study block (JS: base+) // usage of HTML elements to add, wing up & del tasks

--- js/index.js
+++ js/index.js
@@ -1,82 +1,56 @@
-let tasks = ["Выполнить ДЗ","Приготовить ужин", "Покушать"];
-let isCompleted = [false, false, false]
-function showTasks() {
-    if (tasks.length === 0) {
-        alert("У Вас нет задач!");
-    } else {
-        let tasksList = "Ваши задачи:\n";
-        for (let i = 0; i < tasks.length; i++) {
-            tasksList+=tasks[i];
-            isCompleted[i] ? tasksList+=" (выполнено)\n" : tasksList+=" (не выполнено)\n";
-        }
-        alert(tasksList);
+const container = document.getElementById('container')
+const nameInput = document.getElementById('nameInput')
+const addBtn = document.getElementById('addBtn')
+const nameDone = document.getElementById('nameDone')
+const doneBtn = document.getElementById('doneBtn')
+const nameDel = document.getElementById('nameDel')
+const delBtn = document.getElementById('delBtn')
+let tasks =[]
+addBtn.addEventListener('click', () => {
+    const task = {
+        name: nameInput.value,
+        isDone: false
     }
+    tasks.push(task)
+    nameInput.value = ''
+    render()
+})
+
+const render = () => {
+    container.innerHTML = ''
+    tasks.map((task) => {
+        const taskItem = document.createElement('div')
+        taskItem.innerHTML = task.name
+        taskItem.classList.add('task-item')
+
+        const taskIsDone = document.createElement('div')
+        task.isDone ? taskIsDone.innerHTML="(выполнено)" : taskIsDone.innerHTML="(не выполнено)"
+        taskIsDone.classList.add('task-is-done')
+
+        taskItem.appendChild(taskIsDone)
+        container.appendChild(taskItem)
+    })
 }
 
-function addTask() {
-    const newTask = prompt("Введите задачу");
-    if (newTask) {
-        tasks.push(newTask);
-        isCompleted.push(false);
-        showTasks()
-    } else {
-        alert("Вы не ввели задачу!");
-    }
-}
-
-function deleteTask() {
-    showTasks();
-    const nameOfTask = prompt("Укажите название задачи для удаления:");
-    if (tasks.includes(nameOfTask)) {
-        const indexOfDeletedTask = tasks.indexOf(nameOfTask);
-        const deletedTask = tasks.splice(indexOfDeletedTask, 1);
-        isCompleted.splice(indexOfDeletedTask, 1);
-        alert("Вы удалили задачу: " + deletedTask);
-    } else {
-        alert("Некорректное название задачи.");
-    }
-    showTasks();
-}
-
-function completeTask() {
-    showTasks();
-    const nameOfTask = prompt("Укажите название выполненной задачи:");
-    if (tasks.includes(nameOfTask)) {
-        const indexOfCompletedTask = tasks.indexOf(nameOfTask);
-        isCompleted[indexOfCompletedTask] = true;
+doneBtn.onclick = function() {
+    const doneIndex = tasks.findIndex(e => e.name === nameDone.value);
+    if (doneIndex>=0) {
+        tasks[doneIndex].isDone = true
+        nameDone.value = ''
     } else {
         alert("Некорректное название задачи.");
     }
-    showTasks();
+    render()
 }
 
-let choice;
-
-function main() {
-    do {
-        choice = +prompt(
-            "Выберете действие \n1: Показать список задач \n2: Добавить задачу \n3: Пометить задачу как выполненную \n4: Удалить задачу \n0: Выйти из программы"
-        );
-        switch (choice) {
-            case 0:
-                alert("Выход из программы");
-                break;
-            case 1:
-                showTasks();
-                break;
-            case 2:
-                addTask();
-                break;
-            case 3:
-                completeTask();
-                break;
-            case 4:
-                deleteTask();
-                break;
-            default:
-                alert("Некорректный выбор.");
-        }
-    } while (choice);
+delBtn.onclick = function() {
+    const delIndex = tasks.findIndex(e => e.name === nameDel.value);
+    if (delIndex>=0) {
+        tasks.splice(delIndex, 1);
+        nameDel.value = ''
+    } else {
+        alert("Некорректное название задачи.");
+    }
+    render()
 }
 
-main();
